6815310f2a126b10621f294a4960187b84b19258
TheGoodCoder12 <satyam.strange93@gmail.com>
2024-09-06 17:45:37 +0530

    basic initialization done

---
+++ myblog/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      About page goes here
+      {/* This is called file based routing */}
+    </div>
+  )
+}
+
+export default page
